c17e479a85779ad4fd387748b3dc3a7f3f3ea41f
Dmytro Holovskyi <80936904545dgold@gmail.com>
2025-03-24 16:48:46 +0200

    added task-3

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function checkForSpam(message) {
+  const forbiddenWordSpam = 'spam';
+  const forbiddenWordSale = 'sale';
+
+  if (
+    message.toLowerCase().includes(forbiddenWordSale) ||
+    message.toLowerCase().includes(forbiddenWordSpam)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
